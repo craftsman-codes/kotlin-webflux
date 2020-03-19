107b3f4b0764fdd7eddcbd914f04ccbac02b8d3a
Joost Heijkoop <joost@craftsman.codes>
2020-03-19 16:55:23 +0100

    Make websocket work with messages

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,9 +1,26 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect, useMemo, useState} from 'react'
 import {Grid, List, ListItem, Paper, TextField} from "@material-ui/core"
+import useWebSocket from 'react-use-websocket';
 
 export const App = () => {
-  const [messages, setMessages] = useState<{message: string, createdAt: string}[]>([])
+  const [messages, setMessages] = useState<{user: string, message: string, createdAt: string}[]>([])
   const [message, setMessage] = useState<string>('')
+  const [user, setUser] = useState<string>('anonymous')
+
+  const STATIC_OPTIONS = useMemo(() => ({
+    onOpen: () => console.log('opened'),
+    shouldReconnect: (event: WebSocketEventMap['close']) => true,
+    reconnectAttempts: 10,
+    reconnectInterval: 3000,
+  }), []);
+
+  const [sendMessage, lastMessage] = useWebSocket('ws://localhost:8080/socket', STATIC_OPTIONS);
+
+  useEffect(() => {
+    if (lastMessage) {
+      setMessages(prev => [...prev, JSON.parse(lastMessage.data)])
+    }
+  }, [lastMessage])
 
   const load = () => fetch('http://localhost:8080/api/messages')
     .then(response => response.json())
@@ -24,10 +41,17 @@ export const App = () => {
     <Grid container>
       <Grid item lg={9}>
         <List>
-          {messages.map(m => <ListItem key={m.createdAt}>{m.createdAt}: {m.message}</ListItem>)}
+          {messages.map(m => <ListItem key={m.createdAt}>{m.createdAt.slice(11, 19)} [{m.user}]: {m.message}</ListItem>)}
         </List>
       </Grid>
       <Grid item lg={3}>
+        <TextField
+          variant="filled"
+          label="User"
+          autoFocus={true}
+          value={user}
+          onChange={event => setUser(event.target.value)}
+        />
         <TextField
           variant="filled"
           label="Type your message"
@@ -36,7 +60,7 @@ export const App = () => {
           onChange={event => setMessage(event.target.value)}
           onKeyUp={event => {
             if (event.ctrlKey && event.key && message.trim()) {
-              add(message, 'anonymous').then(() => setMessage(''))
+              add(message.trim(), user).then(() => setMessage(''))
             }
           }}
         />
